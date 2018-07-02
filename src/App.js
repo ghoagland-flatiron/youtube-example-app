473fe7b5dfde4db3acbff73b0a656ca76901e87f
ghoagland-flatiron <georgina.hoagland@flatironschool.com>
2018-07-02 10:46:46 -0400

    created starting point

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import YouTubeContainer from './YouTubeContainer'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <YouTubeContainer />
+        Your app goes here!
       </div>
     );
   }
